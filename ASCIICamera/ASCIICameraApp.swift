4f9433086cc93962641bd39fc1cc34b140214da5
Jacopo Gasparetto <jacopo.gasparetto@ammagamma.com>
2022-03-06 18:32:06 +0100

    Initial Commit

---
+++ ASCIICamera/ASCIICameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCIICameraApp.swift
+//  ASCIICamera
+//
+//  Created by Jacopo Gasparetto on 06/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ASCIICameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

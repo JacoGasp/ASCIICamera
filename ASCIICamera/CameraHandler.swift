6f834e919f051aaa742d4ff1759b3e6122a03d3b
Jacopo Gasparetto <jacopo.gasparetto@ammagamma.com>
2022-03-07 09:14:29 +0100

    First commit

---
+++ ASCIICamera/CameraHandler.swift
@@ -0,0 +1,114 @@
+//
+//  CameraHandler.swift
+//  ASCIICamera
+//
+//  Created by Jacopo Gasparetto on 06/03/22.
+//
+
+import AVKit
+import Foundation
+
+class CameraOutput: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    let density = "#####********+++++++++=========--------:::::::::.."
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            CVPixelBufferLockBaseAddress(imageBuffer, .readOnly)
+            let height = CVPixelBufferGetHeight(imageBuffer)
+            let width = CVPixelBufferGetWidth(imageBuffer)
+            let int32PerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+            let pixelFormat = CVPixelBufferGetPixelFormatType(imageBuffer)
+            
+            guard CVPixelBufferGetPixelFormatType(imageBuffer) == kCVPixelFormatType_32BGRA else {
+                print("Wrong pixel format")
+                return
+            }
+            
+            print("frame!")
+            if let bufferAddress = CVPixelBufferGetBaseAddress(imageBuffer) {
+                let pixels = bufferAddress.assumingMemoryBound(to: UInt32.self)
+                
+                for row in 0..<height {
+                    for col in 0..<width {
+                        let luma = pixels[row * width + col]
+                        let blue = (luma >> 8) & 0xFF
+                        let green = (luma >> 16) & 0xFF
+                        let red = (luma >> 24) & 0xFF
+                        let luminosity = Float(red + green + blue) / 3.0
+                        print(red, green, blue)
+                        print(luminosity)
+                    }
+                }
+            }
+            CVPixelBufferUnlockBaseAddress(imageBuffer, .readOnly)
+        }
+    }
+}
+
+class CameraHandler {
+    var captureSession: AVCaptureSession
+    var cameraOutput = CameraOutput()
+    
+    init() {
+        captureSession = AVCaptureSession()
+        setupCamera()
+    }
+    
+    private
+    func setupCaptureSession() {
+        captureSession.beginConfiguration()
+        let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                  for: .video, position: .unspecified)
+        guard
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
+            captureSession.canAddInput(videoDeviceInput)
+            else { return }
+        captureSession.addInput(videoDeviceInput)
+        
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(cameraOutput, queue: .main)
+        videoOutput.videoSettings = [String(kCVPixelBufferPixelFormatTypeKey): kCVPixelFormatType_32BGRA]
+        guard captureSession.canAddOutput(videoOutput) else { return }
+        captureSession.sessionPreset = .hd1280x720
+        captureSession.addOutput(videoOutput)
+        captureSession.commitConfiguration()
+    }
+    
+    public
+    func setupCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized: // The user has previously granted access to the camera.
+                self.setupCaptureSession()
+                break
+            
+            case .notDetermined: // The user has not yet been asked for camera access.
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    if granted {
+                        self.setupCaptureSession()
+                    }
+                }
+            
+            case .denied: // The user has previously denied access.
+                return
+
+            case .restricted: // The user can't grant access due to restrictions.
+                return
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func startSession() {
+            guard !captureSession.isRunning else { return }
+            captureSession.startRunning()
+        }
+
+        func stopSession() {
+            guard captureSession.isRunning else { return }
+            captureSession.stopRunning()
+        }
+    
+    
+}
+

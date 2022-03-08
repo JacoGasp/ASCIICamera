61eeab8ef1cca103e75850c9d1159dadbd8961d2
Jacopo Gasparetto <jacopo.gasparetto@ammagamma.com>
2022-03-08 12:27:16 +0100

    Use TextView and attributed string

--- ASCIICamera/ASCIICameraView.swift
+++ ASCIICamera/ASCIICameraView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Cocoa
 
 struct ASCIICameraView: View {
     @ObservedObject var cameraOutput: CameraOutput
@@ -50,13 +51,60 @@ struct ASCIICameraView: View {
                     }
                 }
             }.padding(8)
-            Text(cameraOutput.text)
-                .font(.system(size: 14, design: .monospaced))
-                .frame(width: 840, height: 800, alignment: .center)
-                ._lineHeightMultiple(0.5)
+            TextWithAttributes(attributedString: formatText(cameraOutput.text))
                 .onChange(of: selectedDensity, perform: { newValue in
                     cameraOutput.density = Array(newValue)
                 })
         }
+    }
+    
+    func formatText(_ text: String) -> NSAttributedString {
+        let formattedText = NSMutableAttributedString(string: text)
+        let range = text.startIndex ..< text.endIndex
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = 0.5
+        paragraphStyle.alignment = .center
+        formattedText.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(range, in: text))
+        return formattedText
+    }
+}
+
+
+class FormattedLabelView : NSView {
+    private var label = NSTextField()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.isEditable = false
+        label.autoresizingMask = [.width, .height]
+        label.maximumNumberOfLines = -1
+        label.backgroundColor = .clear
+        label.font = NSFont(name: "Courier", size: 14)
+        
+        label.isBordered = false
+        self.addSubview(label)
+        self.autoresizesSubviews = true
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setString(_ attributedString: NSAttributedString) {
+        self.label.attributedStringValue = attributedString
+    }
+}
+
+struct TextWithAttributes: NSViewRepresentable {
+    var attributedString: NSAttributedString
+    
+    func makeNSView(context: Context) -> FormattedLabelView {
+        let view = FormattedLabelView(frame: .zero)
+        return view
+    }
+    
+    func updateNSView(_ nsView: FormattedLabelView, context: Context) {
+        nsView.setString(attributedString)
     }
 }
